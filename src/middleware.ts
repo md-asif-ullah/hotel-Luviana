67e4eb3ef8d22b885f3d504108f431485f50e1a3
md-asif-ullah <mdasifullah456@gmail.com>
2024-09-28 07:31:10 -0700

    update refresh token functionality

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-import type { NextFetchEvent, NextRequest } from "next/server";
-import { jwtVerify } from "jose";
+import type { NextRequest } from "next/server";
+import { jwtVerify, SignJWT } from "jose";
 
 interface JwtPayload {
   user: {
@@ -8,9 +8,38 @@ interface JwtPayload {
   };
 }
 
-export async function middleware(request: NextRequest, event: NextFetchEvent) {
+export async function middleware(request: NextRequest) {
   const token = request.cookies.get("access_token")?.value;
+  const refreshToken = request.cookies.get("refresh_token")?.value;
   const { pathname } = request.nextUrl;
+
+  if (!token && refreshToken) {
+    try {
+      // Verify the refresh token
+      const secret = new TextEncoder().encode(process.env.JWT_REFRESH_SECRET);
+      const { payload } = (await jwtVerify(refreshToken, secret)) as {
+        payload: JwtPayload;
+      };
+
+      // Generate a new access token
+      const newAccessToken = await new SignJWT({ user: payload.user })
+        .setProtectedHeader({ alg: "HS256" })
+        .setExpirationTime("1h")
+        .sign(new TextEncoder().encode(process.env.JWT_REFRESH_SECRET));
+
+      // Set the new access token in the response cookies
+      const response = NextResponse.next();
+      response.cookies.set("access_token", newAccessToken, {
+        httpOnly: true,
+        path: "/",
+        maxAge: 60 * 60 * 24 * 1,
+      });
+
+      return response;
+    } catch (error) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  }
 
   let isAdmin;
 
@@ -32,6 +61,7 @@ export async function middleware(request: NextRequest, event: NextFetchEvent) {
     pathname === "/login" ||
     pathname === "/signup" ||
     pathname.startsWith("/verify");
+
   const userPaths =
     pathname.startsWith("/rooms/") ||
     pathname === "/deshboard/profile" ||
@@ -57,5 +87,5 @@ export async function middleware(request: NextRequest, event: NextFetchEvent) {
 }
 
 export const config = {
-  matcher: ["/rooms/:id*", "/login", "/signup", "/verify", "/deshboard/:path*"],
+  matcher: ["/:path*"],
 };
