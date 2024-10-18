d7a801573b6b55f10d75276e8a27be940ef5939c
md-asif-ullah <mdasifullah456@gmail.com>
2024-10-18 02:58:04 -0700

    update admin rooms funtionality

--- src/app/dashboard/(admin)/rooms/page.tsx
+++ src/app/dashboard/(admin)/rooms/page.tsx
@@ -20,105 +20,20 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { useCallback, useEffect, useState } from "react";
-import axios from "axios";
-import { IPaginationTypes, RoomTypes } from "@/types";
 import { formateData } from "@/lib/utils";
 import PageLoadingAnimation from "@/components/PageLoadingAnimation";
 import DataTable from "@/components/DataTable";
 import ErrorPage from "@/components/ErrorPage";
 import RoomsTableActions from "@/components/RoomsTableActions";
 import Image from "next/image";
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
+import { IPaginationTypes, RoomTypes } from "@/types";
 
-interface IRoomResponseTypes {
+export interface IRoomResponseTypes {
   rooms: RoomTypes[];
   pagination: IPaginationTypes;
 }
-
-const columns: ColumnDef<RoomTypes>[] = [
-  {
-    accessorKey: "createdAt",
-    header: "Created Date",
-    cell: ({ row }) => {
-      const date = row.getValue<string>("createdAt");
-      const formattedDate = formateData(date);
-
-      return <div>{formattedDate}</div>;
-    },
-  },
-  {
-    accessorKey: "roomName",
-    header: "Room",
-    cell: ({ row }) => {
-      const roomName = row.original.roomName;
-      const images = row.original.images;
-
-      return (
-        <div className="flex items-center space-x-4">
-          <Image
-            src={images[0]}
-            alt={roomName}
-            width={50}
-            height={50}
-            className="w-[50px] h-[50px] rounded-xl object-cover"
-          />
-          <h3 className="whitespace-nowrap font-medium text-md">{roomName}</h3>
-        </div>
-      );
-    },
-  },
-  {
-    accessorKey: "categories",
-    header: "Categories",
-
-    cell: ({ row }) => <div>{row.getValue("categories")}</div>,
-  },
-  {
-    accessorKey: "price",
-    header: "Price",
-    cell: ({ row }) => {
-      return <div>{row.getValue<string>("price")}</div>;
-    },
-  },
-
-  {
-    accessorKey: "adults",
-    header: "Adults",
-    cell: ({ row }) => {
-      return <div>{row.getValue("adults")}</div>;
-    },
-  },
-  {
-    accessorKey: "children",
-    header: "Children",
-    cell: ({ row }) => {
-      return <div>{row.getValue("children")}</div>;
-    },
-  },
-  {
-    id: "actions",
-    enableHiding: false,
-    cell: ({ row }) => {
-      const room = row.original;
-
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <RoomsTableActions id={room._id} />
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
-    },
-  },
-];
 
 function Rooms() {
   const [sorting, setSorting] = React.useState<SortingState>([]);
@@ -134,7 +49,7 @@ function Rooms() {
   const [isLoading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string>("");
 
-  const getCustomers = useCallback(async () => {
+  const getRooms = useCallback(async () => {
     setLoading(true);
     try {
       const res = await axios.get(
@@ -154,11 +69,98 @@ function Rooms() {
 
   useEffect(() => {
     const debounceSearch = setTimeout(() => {
-      getCustomers();
+      getRooms();
     }, 1000);
 
     return () => clearTimeout(debounceSearch);
-  }, [search, page, getCustomers]);
+  }, [search, page, getRooms]);
+
+  const columns: ColumnDef<RoomTypes>[] = [
+    {
+      accessorKey: "createdAt",
+      header: "Created Date",
+      cell: ({ row }) => {
+        const date = row.getValue<string>("createdAt");
+        const formattedDate = formateData(date);
+
+        return <div>{formattedDate}</div>;
+      },
+    },
+    {
+      accessorKey: "roomName",
+      header: "Room",
+      cell: ({ row }) => {
+        const roomName = row.original.roomName;
+        const images = row.original.images;
+
+        return (
+          <div className="flex items-center space-x-4">
+            <Image
+              src={images[0]}
+              alt={roomName}
+              width={50}
+              height={50}
+              className="w-[50px] h-[50px] rounded-xl object-cover"
+            />
+            <h3 className="whitespace-nowrap font-medium text-md">
+              {roomName}
+            </h3>
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "categories",
+      header: "Categories",
+
+      cell: ({ row }) => <div>{row.getValue("categories")}</div>,
+    },
+    {
+      accessorKey: "price",
+      header: "Price",
+      cell: ({ row }) => {
+        return <div>{row.getValue<string>("price")}</div>;
+      },
+    },
+
+    {
+      accessorKey: "adults",
+      header: "Adults",
+      cell: ({ row }) => {
+        return <div>{row.getValue("adults")}</div>;
+      },
+    },
+    {
+      accessorKey: "children",
+      header: "Children",
+      cell: ({ row }) => {
+        return <div>{row.getValue("children")}</div>;
+      },
+    },
+    {
+      id: "actions",
+      enableHiding: false,
+      cell: ({ row }) => {
+        const room = row.original;
+
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <RoomsTableActions id={room._id} getRooms={getRooms} />
+            </DropdownMenuContent>
+          </DropdownMenu>
+        );
+      },
+    },
+  ];
 
   const table = useReactTable({
     data: data?.rooms ?? [],
@@ -188,7 +190,9 @@ function Rooms() {
 
   return (
     <div className="pb-20 min-h-screen pt-10 w-full h-full bg-white px-6">
-      <h2 className="text-start font-semibold text-2xl md:text-3xl">Rooms</h2>
+      <h2 className="text-start font-semibold text-2xl md:text-3xl">
+        Rooms List
+      </h2>
       <DataTable
         table={table}
         page={page}
--- src/components/RoomsTableActions.tsx
+++ src/components/RoomsTableActions.tsx
@@ -5,48 +5,51 @@ import { useToast } from "./ui/use-toast";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
-import Swal from "sweetalert2";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
-function RoomsTableActions({ id }: { id: string }) {
+interface IRoomTableActionsPropsTypes {
+  id: string;
+  getRooms: () => Promise<void>;
+}
+
+function RoomsTableActions({ id, getRooms }: IRoomTableActionsPropsTypes) {
   const { toast } = useToast();
   const router = useRouter();
 
-  const handleDeleteRoom = () => {
-    Swal.fire({
-      title: "Are you sure?",
-      text: "You won't be able to revert this!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!",
-    }).then(async (result) => {
-      if (result.isConfirmed) {
-        try {
-          const res = await axios.delete(`/api/rooms/${id}`);
-          if (res.data.success) {
-            toast({
-              title: "Success",
-              description: "Room deleted successfully",
-            });
-            router.refresh();
-          }
-          if (!res.data.success) {
-            toast({
-              variant: "destructive",
-              title: "Error",
-              description: "Failed to delete room",
-            });
-          }
-        } catch (error) {
-          toast({
-            variant: "destructive",
-            title: "Error",
-            description: "Failed to delete room",
-          });
-        }
+  const handleDeleteRoom = async () => {
+    try {
+      const res = await axios.delete(`/api/rooms/${id}`);
+      if (res.data.success) {
+        toast({
+          title: "Success",
+          description: "Room deleted successfully",
+        });
+        await getRooms();
       }
-    });
+      if (!res.data.success) {
+        toast({
+          variant: "destructive",
+          title: "Error",
+          description: "Failed to delete room",
+        });
+      }
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: "Failed to delete room",
+      });
+    }
   };
 
   return (
@@ -57,9 +60,32 @@ function RoomsTableActions({ id }: { id: string }) {
       <DropdownMenuItem>
         <Link href={`/dashboard/rooms/edit-room/${id}`}>Edit</Link>
       </DropdownMenuItem>
-      <DropdownMenuItem>
-        <button onClick={handleDeleteRoom}>Delete</button>
-      </DropdownMenuItem>
+
+      <div className="hover:bg-[#f1f2f3] duration-500">
+        <Dialog>
+          <DialogTrigger asChild>
+            <button className="text-sm ml-2">Delete</button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px] bg-white shadow-lg rounded-md p-6">
+            <DialogHeader>
+              <DialogTitle className="text-lg font-semibold">
+                Confirm Delete
+              </DialogTitle>
+              <DialogDescription className="text-gray-600">
+                Are you sure you want to delete this item permanently?
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter className="flex justify-end space-x-2 mt-2">
+              <DialogClose asChild>
+                <Button variant="secondary">Cancel</Button>
+              </DialogClose>
+              <Button variant="destructive" onClick={handleDeleteRoom}>
+                Delete
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </div>
     </>
   );
 }
