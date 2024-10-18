01894167aeb446bc5f07829cb7dab136f237161e
md-asif-ullah <mdasifullah456@gmail.com>
2024-10-18 09:26:41 -0700

    update deshboard navbar

--- src/components/DashboardNavbar.tsx
+++ src/components/DashboardNavbar.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import UserAccount from "./UserAccount";
+import { useAuth } from "./hooks/useAuth";
+import { Separator } from "@radix-ui/react-separator";
 
 function DashboardNavbar() {
+  const { user } = useAuth();
+
   return (
-    <nav className="flex justify-between h-20 py-4 px-5 md:px-10 border-b border-[#e2e8f0]">
+    <nav className="flex justify-between items-center h-20 py-4 px-5 md:px-10 border-b border-[#e2e8f0]">
       <Link href="/" className="flex items-center">
         <Image
           src={"/assets/icons/logotype_gold.svg"}
@@ -16,7 +22,33 @@ function DashboardNavbar() {
         <h1 className={"text-2xl ml-4 text-black"}>Luviana</h1>
       </Link>
 
-      <UserAccount />
+      <div>
+        {/* user account for desktop */}
+
+        <div className="hidden lg:block">
+          {user ? (
+            <div className="md:flex items-center hidden space-x-1">
+              <UserAccount />
+            </div>
+          ) : (
+            <div className="sm:flex hidden h-5 items-center space-x-4 text-sm">
+              <Link href="/login" className="text-base">
+                Login
+              </Link>
+              <Separator orientation="vertical" />
+              <Link href="/register" className="text-base">
+                Register
+              </Link>
+            </div>
+          )}
+        </div>
+
+        {/* user account for mobile */}
+
+        <div className="lg:hidden">
+          <UserAccount />
+        </div>
+      </div>
     </nav>
   );
 }
